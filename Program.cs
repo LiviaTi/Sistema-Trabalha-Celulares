58adc78a504bb331002e5f8e96a5aaeecc8969d4
liviaTI <liviasantferreira@gmail.com>
2025-06-04 16:51:53 -0300

    Fazendo as devidas correções no código

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia(numero:"38A934", modelo:"Nokia C2", imei:"1235HJ", memoria:128);
+
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Spotify");
+
+
+Iphone i1 = new Iphone(numero:"23ACD43", modelo:"Iphone 15", imei:"7635HJ", memoria:256);
+
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Tinder");
